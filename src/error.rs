9168b48d589bc29a608afbd53a702fac39e8efeb
niklasanesen <anesen.niklas@gmail.com>
2026-09-20 07:26:39 +0200

    error fix

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,30 @@
+use askama::Template;
 use axum::{
     http::StatusCode,
-    response::{IntoResponse, Response},
+    response::{Html, IntoResponse, Response},
 };
 
 pub struct AppError(anyhow::Error);
 
+#[derive(Template)]
+#[template(ext = "html", source = r#"<p class="text-red">{{message}}</p>"#)]
+struct ErrorTemplate {
+    message: String,
+}
+
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Html(
+                ErrorTemplate {
+                    message: self.0.to_string(),
+                }
+                .render()
+                .unwrap_or_else(|_| "something went wrong".to_owned()),
+            ),
+        )
+            .into_response()
     }
 }
 
